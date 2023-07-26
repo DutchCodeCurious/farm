0d42acde5e891bef6fcce3eda64142ef2c79ded0
DutchCodeCurious <114599908+DutchCodeCurious@users.noreply.github.com>
2023-07-26 13:25:17 +0200

    First commit

---
+++ test_site.py
@@ -0,0 +1,9 @@
+from pytest import requests
+
+
+def test_website_online():
+    url = "159.89.8.96"
+
+    response = requests.get(url)
+
+    assert response.status_code == 200, f"De website {url} is offline. Statuscode: {response.status_code}"
